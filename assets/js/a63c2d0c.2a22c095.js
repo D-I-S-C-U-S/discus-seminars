fd91e57a6487874f93da8d75a4ad604861f017b7
olliethomas <1835492+olliethomas@users.noreply.github.com>
2021-06-24 09:38:43 +0000

    Deploy website - based on 0254c5a4f252e98222434f883b3149b1f3be627e

--- assets/js/a63c2d0c.2a22c095.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdiscus_seminars=self.webpackChunkdiscus_seminars||[]).push([[838],{6353:function(e){"use strict";e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Seminar - ML for TTZ","permalink":"/seminar1"},{"title":"Hello, World!","permalink":"/starting"}]}')}}]);
